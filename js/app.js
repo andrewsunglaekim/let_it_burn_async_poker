2efd649243f265c67624c1fded6ddb7898688f8c
Andy <andrew.sunglae.kim@gmail.com>
2016-04-01 14:06:48 -0400

    finishes straight without ace as low card

--- js/app.js
+++ js/app.js
@@ -9,6 +9,14 @@ $(document).ready(function(){
   dealer.dealFlop()
   dealer.dealTurn()
   dealer.dealRiver()
+  testCard6 = new Card("spades", "8")
+  testCard = new Card("spades", "9")
+  testCard1 = new Card("spades", "10")
+  testCard2 = new Card("spades", "J")
+  testCard3 = new Card("spades", "Q")
+  testCard4 = new Card("spades", "5")
+  testCard5 = new Card("spades", "K")
+  dealer.board = [testCard, testCard1, testCard2, testCard3, testCard4, testCard5, testCard6]
   decider = new Decider(dealer)
 
 })
--- js/models/decider.js
+++ js/models/decider.js
@@ -30,7 +30,6 @@ Decider.prototype = {
 
   },
   isStraight: function(hand){
-    // order the hand
     var orderedHand = _.sortBy(hand, function(card){ return RANKS.indexOf(card.rank)})
     var straightHand = []
     straightHand.push(orderedHand[0])
@@ -39,12 +38,12 @@ Decider.prototype = {
       var cardRank = RANKS.indexOf(card.rank)
       var lastCardInStraightRank = RANKS.indexOf(straightHand[straightHand.length - 1].rank)
       var isCardOneHigher = cardRank - 1 == lastCardInStraightRank
-      // console.log(RANKS.indexOf(card.rank) - 1)
-      console.log(orderedHand[i].rank)
       if (!isCardOneHigher && straightHand.length < 5) {
         straightHand = []
+        straightHand.push(card)
+      } else if (isCardOneHigher) {
+        straightHand.push(card)
       }
-      straightHand.push(card)
       straightHand = _.uniq(straightHand, function(card){
         return card.rank
       })
